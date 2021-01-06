36801d9dd5224c27c8f67a4030eb468377656064
LuzianeFreitas <freitas.lu@outlook.com>
2021-01-06 13:24:36 -0300

    feat: add function
    generates random positions

--- jogo.js
+++ jogo.js
@@ -5,8 +5,28 @@ var largura = 0
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
-
-    console.log(altura, largura);
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+    // Gerar posições aleatórias para o mosquito com base no taanho disponível da tela
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    // Criando o elemento (mosquito) em HTML
+    var mosquito = document.createElement('img')
+
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+posicaoRandomica()
